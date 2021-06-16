799adc1135278b69080dcdc13abcc7ed81869571
Oluwatimi11 <57862887+Oluwatimi11@users.noreply.github.com>
2021-06-16 22:09:41 +0100

    Landing Page
    Almost completed

---
+++ css/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/icon-font.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="sass/main.scss">
+        <link rel="shortcut icon" type="image.png" href="img/THE ANCESTORS.png">
+
+        <title> RealityTV | RealityTVregistry.com</title>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="section-one">
+              <div class="social-links">
+                <div class="facebook">
+                  <span> LOGIN WITH FACEBOOk </span>
+                  <div class="icon">
+                    <img src="./assets/facebookLogo.svg" alt="" />
+                  </div>
+                </div>
+                <div class="twitter">
+                  <div class="icon">
+                    <img src="./assets/twitterLogo.svg" alt="" srcset="" />
+                  </div>
+                  <span> LOGIN WITH TWITTER </span>
+                </div>
+              </div>
+              <div class="main-form">
+                <input type="email" name="email" placeholder="Email" />
+                <input type="password" name="password" placeholder="Password" />
+                <a href="#">I forgot my password?</a>
+                <button>Login</button>
+              </div>
+            </div>
+      
+            <div class="section-two">
+              <div class="new-account">
+                <button>Create New Account</button>
+              </div>
+            </div>
+          </div>
+    </body>
+</html>    
---
+++ scripts/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/icon-font.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="sass/main.scss">
+        <link rel="shortcut icon" type="image.png" href="img/logo.png">
+        <script type="text/javascript" src="js/reality.js"></script>
+
+        <title> RealityTV | RealityTVregistry.com</title>
+
+    </head>
+    <body>
+        <main>
+                <header>
+                        <div class="header">
+                            <nav class="navigation__nav">
+                                <div class="navbar__logo-box">
+                                    <img src="img/logo.png" alt="Logo" class="header__logo">
+                                </div>
+                                <ul class="navigation__list">
+                                    <li class="navigation__item"><a href="#" class="navigation__link">Home</a></li>
+                                    <li class="navigation__item"><a href="aboutus.html" class="navigation__link">About Us</a></li>
+                                    <li class="navigation__item"><a href="contactus.html" class="navigation__link">Contact Us</a></li>
+                                    <li class="navigation__item"><a href="login.html" class="btn btn--white btn--animated">SignUp/Login</a></li>
+                                </ul>              
+                            </nav>
+                                <div class="header__text-box">
+                                    <h1 class="heading-primary">
+                                        <span class="heading-primary--main">Reality Tv</span>
+                                        <span class="heading-primary--sub">cache of unique formats & talent</span>
+                                    </h1>        
+                                </div>
+                        </div>  
+                </header>
