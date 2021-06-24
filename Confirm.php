dcd6479e47f5ca16c9cc94cf63297a533ffc8a4f
Oluwatimi11 <57862887+Oluwatimi11@users.noreply.github.com>
2021-06-24 11:54:52 +0100

    Projects and Confirm
    Projects and confirm changes

---
+++ Confirm.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/icon-font.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="sass/main.scss">
+        <link rel="shortcut icon" type="image.png" href="img/logo.png">
+
+        <script type="text/javascript" src="js/reality.js"></script>
+
+        <title> RealityTV | RealityTVregistry.com</title>
+
+    </head>
+    <body>
+      <main>
+          <div class="signup__container">
+
+              <div class="signup__logo">
+                  <div class="section-one">
+                      <img src="img/logo.png" alt="Logo" class="signup__logo-box" height="75rem" width="180rem">
+                  </div>
+              </div>
+
+            <div class="section-two">
+              <p class="signup__container--text"> Thank You for joinig us. </br> Kindly check your email to confirm and complete the Sign Up Process.</p>
+            </div>
+      </main>
+  </body>
+</html>
--- scripts/header.php
+++ scripts/header.php
@@ -7,7 +7,6 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <link rel="stylesheet" href="css/icon-font.css">
         <link rel="stylesheet" href="css/style.css">
@@ -21,16 +20,16 @@
     <body>
         <main>
                 <header>
+                        <!-- <div class="navbar__logo-box">
+                            <img src="img/logo.png" alt="Logo" class="header__logo">
+                        </div>
                         <div class="header">
                             <nav class="navigation__nav">
-                                <div class="navbar__logo-box">
-                                    <img src="img/logo.png" alt="Logo" class="header__logo">
-                                </div>
                                 <ul class="navigation__list">
                                     <li class="navigation__item"><a href="#" class="navigation__link">Home</a></li>
                                     <li class="navigation__item"><a href="aboutus.html" class="navigation__link">About Us</a></li>
                                     <li class="navigation__item"><a href="contactus.html" class="navigation__link">Contact Us</a></li>
-                                    <li class="navigation__item"><a href="login.php" class="btn btn--white btn--animated">SignUp/Login</a></li>
+                                    <li class="navigation__item"><a href="SignUp.php" class="btn btn--white btn--animated">SignUp/Login</a></li>
                                 </ul>              
                             </nav>
                                 <div class="header__text-box">
@@ -39,5 +38,78 @@
                                         <span class="heading-primary--sub">cache of unique formats & talent</span>
                                     </h1>        
                                 </div>
-                        </div>  
+                        </div>   -->
+
+                                <div class="navbar__logo">
+                                    <img src="img/logo.png" alt="Reality Tv Logo" class="navbar__logo-box">
+                                </div>
+
+                                <nav class="clearfix">
+                                    <div class="navbar">
+                                        <div class="dropdown">
+                                            <button class="dropbtn">Home
+                                            <i class="fa fa-caret-down"></i>
+                                            </button>
+                                        </div>
+                                        <div class="dropdown">
+                                            <button class="dropbtn">Contestants,Actors,Models and Musicians Registrations
+                                                <i class="fa fa-caret-down"></i>
+                                            </button>
+                                            <div class="dropdown-content">
+                                                <a href="#" class="navbar_item">Privacy Policy - Contestant, Talent and Executive</a>
+                                            </div>
+                                        </div>
+                                        <div class="dropdown">
+                                            <button class="dropbtn">Writer/Author Submission Registration
+                                            <i class="fa fa-caret-down"></i>
+                                            </button>
+                                            <div class="dropdown-content">
+                                                <a href="" class="navbar_item">NDA - Confidentiality Agreement</a>
+                                                <a href="" class="navbar_item">Writer/Author - Material Release | Terms of Service</a>
+                                                <a href="" class="navbar_item">Writer Tips and Pitching</a>
+
+                                            </div>
+                                        </div>
+                                        <div class="dropdown">
+                                            <button class="dropbtn">Executive Registration
+                                            <i class="fa fa-caret-down"></i>
+                                            </button>
+                                            <div class="dropdown-content">
+                                                <a href="" class="navbar_item">Current Talent and Submissions</a>
+                                                <a href="" class="navbar_item">Industry Executive Review of Reality TV Proposals</a>
+                                                <a href="" class="navbar_item">Privacy Policy - Executives and Contestants</a>
+
+                                            </div>
+                                        </div>
+                                        <div class="dropdown">
+                                            <button class="dropbtn">About Us
+                                            <i class="fa fa-caret-down"></i>
+                                            </button>
+                                            <div class="dropdown-content">
+                                                <a href="" class="navbar_item">Blog</a>
+                                                <a href="" class="navbar_item">FAQ's</a>
+                                                <a href="" class="navbar_item">Promoting You</a>
+                                                <a href="" class="navbar_item">Talent Areas</a>
+                                                <a href="" class="navbar_item">Writer Benefits and Talent Benefits</a>
+                                            </div>
+                                        </div>
+                                        <div class="dropdown">
+                                            <button class="dropbtn">Contact Us
+                                            <i class="fa fa-caret-down"></i>
+                                            </button>
+                                        </div>
+                                        <div class="buttons">
+                                            <a href="signup.php" class="btn btn--white">SignUp</a>
+                                        </div>
+                                    </div>
+                                </nav>
+
+                            <div class="header">
+                                <div class="header__text-box">
+                                    <h1 class="heading-primary">
+                                        <span class="heading-primary--main">Reality Tv</span>
+                                        <span class="heading-primary--sub">cache of unique formats & talent</span>
+                                    </h1>        
+                                </div>
+                            </div>  
                 </header>
