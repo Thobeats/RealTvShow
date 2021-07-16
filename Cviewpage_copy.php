79881156a31dbdc2c706e634f07cb4c882f25409
firstlincoln <firstlincoln@macs-MacBook-Pro.local>
2021-07-16 12:31:22 +0100

    update

---
+++ Cviewpage_copy.php
@@ -0,0 +1,455 @@
+<?php 
+$navBar = true;
+
+require "scripts/header_two.php";
+
+?>
+
+
+<style>
+.movie-dets{
+    padding : 0px 15px;
+    margin : 0px 15px;
+}
+.cover-image{
+    background-image: url(../img/Onboard1.jpg);
+    height: 100vh;
+    background-size : cover;
+    background-position : top left;
+    color : white;
+    display: flex;
+    align-items : center;
+}
+
+.cover-wrapper{
+    text-align : center;
+    font-family: "Montserrat", sans-serif;
+}
+
+.cover-wrapper h1{
+    font-weight : 600;
+    letter-spacing : 1.75rem;
+    text-transform : uppercase;
+}
+
+.cover-wrapper p{
+    font-weight : 300;
+    letter-spacing : 1rem;
+    text-transform : uppercase;
+    width : 600px;
+}
+
+.zilla{
+    font-family: 'Zilla Slab', serif;    
+}
+
+.poppins{
+    font-family: 'Poppins', sans-serif;
+}
+.movie-header{
+    font-family: 'Zilla Slab', serif;    
+    font-size : 60px;
+    font-weight : 800;
+    width : 250px;
+    text-align : left;
+    color : #041e3c;
+    letter-spacing : 2px;
+    text-transform : uppercase;
+}
+
+.movie-title{
+    padding : 2px;
+    font-family: 'Zilla Slab', serif;    
+    font-size : 20px;
+    font-weight : 300;
+    color : #041e3c;
+    letter-spacing : 2px;
+}
+
+.logline{
+    position : relative;
+    top : 70px;
+}
+
+.logline-header{
+    font-family: 'Zilla Slab', serif;    
+    font-size : 19px;
+    font-weight : 600;
+    letter-spacing : 2px;
+}
+
+.logline-para{
+    padding-top : 10px;
+    font-family: 'Zilla Slab', serif;    
+    font-size : 15px;
+    font-weight : 400;
+    letter-spacing : 2px;
+}
+
+.features {
+    list-style-type : none;
+    padding : 10px;
+    font-family: 'Zilla Slab', serif;   
+    letter-spacing : 2px;
+}
+
+.features-list{
+    margin : 2px;
+    width : 120%;
+}
+
+.features-title{
+    font-weight : 700;
+    font-size: 20px;
+}
+
+.features-detail{
+    width : 700px;
+}
+
+.member-benefits{
+    padding : 20px 0px;
+}
+
+.card{
+    background-color : inherit;
+}
+
+.quotes{
+    padding-top : 10px;
+    padding-right : 10px;
+    font-size: 28px;
+    width: 50%;
+    color : #004883;
+    font-family: 'Poppins', sans-serif;
+    font-weight : 400;
+    letter-spacing : 3px;
+}
+
+.m-title{
+    font-size : 35px;
+    letter-spacing : 2px;
+    font-weight : 800;
+}
+
+</style>
+        <section class="cover-image p-4">
+             <div class="cover-wrapper m-auto d-flex flex-column justify-content-center">                
+                <h1>Reality Tv</h1>
+                <p class="mr-auto ml-auto">cache of unique formats & talent</p>                  
+            </div>
+        </section>
+            <section class="row my-4 p-2 movie-dets">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="d-flex justify-content-center">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-12 col-sm-12 p-5">
+                                <h1 class="movie-header pt-2">
+                                    Title
+                                </h1>
+                                
+                                <p class="movie-title mt-3">
+                                    Epic - Battles of Foreign Lands (Proposed filming in the US)
+                                </p>
+                            
+                            </div> 
+
+                            <div class="col-lg-9 col-md-12 col-sm-12">
+                                <div class="logline p-2">
+                                    <h3 class="logline-header">
+                                        Logline
+                                        <hr>
+                                    </h3>
+
+                                    <p class="logline-para">
+                                        A troop of 30 Roman Soldiers are led to battle against 30 Celti Gauls in this historically
+                                        significant encounter that occured, circa 525 BC. Among the destruction and burning ruins of a
+                                        Roman settlement, the Celtics are observed boasting until they see a troop of Roman Soldiers
+                                        charging at them. Combat is certain snd an Epic Battle of revenge ensues. A man-to-man clash of
+                                        soldiers wielding gladius type weaponry soon intensifies as antiquated pistols are drawn. In the end
+                                        either the Roman Soldiers or Celtic Gauls, will be declared victorious, and advance to battle again.
+                                    
+                                    </p>
+
+                                    <p class="note"><i class="fa fa-thumb-tack" aria-hidden="true"></i>   <b>Note:</b> Proprietary "BattleSafeWeaponry" is specially designed to insure non-injury conflicts)</p>
+
+                                </div> 
+                            </div>
+                                                  
+                        </div>
+                    
+                    </div>
+                
+                </div>
+
+
+                <div class="col-lg-12 col-md-12 col-sm-12 mt-4">
+                    
+                        <div class="row">
+                            <div class="col-lg-7 col-md-12 col-sm-12">
+                                <ul class="features p-2">
+
+                                    <li class="features-list" >
+                                        <span class="features-title">
+                                            Proposal:
+                                        </span>
+
+                                        <span class="features-detail">
+                                        Epic - Battles on Foreign Lands [in the US]
+                                        
+                                        </span>
+
+
+                                    </li>
+
+
+                                    <li class="features-list" >
+                                        <span class="features-title">
+                                            Copyright:
+                                        </span>
+
+                                        <span class="features-detail">
+                                        US Copywright Office Title 17 - April 27, 2021
+                                        
+                                        </span>
+
+
+                                    </li>
+
+
+                                    <li class="features-list" >
+                                        <span class="features-title">
+                                            Reality:
+                                        </span>
+
+                                        <span class="features-detail">
+                                        Unscripted Format/12-episodes arc series
+                                        
+                                        </span>
+
+
+                                    </li>
+
+                                    <li class="features-list" >
+                                        <span class="features-title">
+                                           Purpose:
+                                        </span>
+
+                                        <span class="features-detail">
+                                        Filming pre-production sizzle reels & qualifying
+                                        
+                                        </span>
+
+
+                                    </li>
+
+                                    <li class="features-list" >
+                                        <span class="features-title">
+                                            When / Where:
+                                        </span>
+
+                                        <span class="features-detail">
+                                        July 2021 - Los Angeles and Las Vegas
+                                        
+                                        </span>
+
+
+                                    </li>
+
+                                    <li class="features-list" >
+                                        <span class="features-title">
+                                            Pay range:
+                                        </span>
+
+                                        <span class="features-detail">
+                                        Pre-production $50.00/hr. 
+                                        </span>
+
+
+                                    </li>
+
+                                    <li class="features-list" >
+                                        <span class="features-title">
+                                            Limited to:
+                                        </span>
+
+                                        <span class="features-detail">
+                                       350 propective contestants
+                                        
+                                        </span>
+
+
+                                    </li>
+
+                                    
+                                
+
+                                </ul> 
+                            </div>
+
+                            <div class="col-lg-5 col-md-12 col-sm-6">
+                                <img src="img/war.jpg" width="80%">
+                            </div>
+                                                  
+                        </div>
+                        <hr>
+                 
+                
+                </div>
+
+                <div class="col-12">
+                    <div class="d-flex justify-content-center">
+                        <p class="mt-4 logline-para">
+                            Seeking male and female, rough and tumble types to compete in real battle like clashes that
+                            transcend the usual Realty programming. Fitness and good marksmanship are a plus. You compete
+                            as either a Roman or Celtic. Replication of battle gladius (short swords) and chest armor were 
+                            specifically designed for this program and tested to be battle safe for non-injury contact. 
+                        </p>
+                    </div>
+                </div>
+
+        
+
+                <div class="col-lg-12 col-md-12 col-sm-12 mt-4">
+                    <div class="d-flex justify-content-center mb-4">
+                        <div class="row">                           
+                            <div class="col-lg-4 col-md-4 col-sm-12 mt-3 text-center">
+                                <img src="img/war.jpg" width="80%">
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-sm-12 mt-3 text-center">
+                               <h4 class="text-center zilla">Join our team today!</h4>
+                               <a href="" class="btn btn-warning mt-2">EZ $55 to Register</a>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-sm-12 mt-3 text-center">
+                                <img src="img/vikings.jpg" width="80%">
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 mt-4">
+                    <div class="d-flex justify-content-center">
+                        <div class="row">                           
+                            <div class="col-lg-12 col-md-4 col-sm-12 mt-3 text-center" style="text-transform: uppercase;">
+                                <h4 class="text-center zilla"> As a Registered Member, We Campaign for Your Success</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 mt-4">
+                    <div class="member-benefits">
+                        <h3 class="text-center my-4 m-title">MEMBER BENEFITS</h3>
+                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <div class="card border-0">
+                                        <div class="card-body d-flex justify-content-center">
+                                            <div class="quotes zilla text-center">
+                                           
+                                                Big 93% advantage to be casted when promoted by a business 
+                                              
+                                            </div>
+                                               <img class="" src="img/write01.jpg" width="350px">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="card border-0">
+                                        <div class="card-body d-flex justify-content-center">
+                                            <div class="quotes text-center">
+                                            
+                                                Priority status to participate in the production of your choice
+                                                                               
+                                            </div>
+                                               <img class="" src="img/shoot2.jpg" width="350px">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="card border-0">
+                                        <div class="card-body d-flex justify-content-center">
+                                            <div class="quotes text-center">
+                                      
+                                                Your private account access, to view and edit your credentials 
+                                                                                 
+                                            </div>
+                                               <img class="" src="img/Executive.jpg" width="350px">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="card border-0">
+                                        <div class="card-body d-flex justify-content-center">
+                                            <div class="quotes text-center">
+                                          
+                                                Promote your individual talents to producers and TV executives 
+                                                                                 
+                                            </div>
+                                               <img class="" src="img/act000.jpg" width="350px">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="card border-0">
+                                        <div class="card-body d-flex justify-content-center">
+                                            <div class="quotes text-center">
+                                           
+                                                Casting companies access your info. and initiate contact
+                                                                                     
+                                            </div>
+                                               <img class="" src="img/studio0.jpg" width="350px">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="card border-0">
+                                        <div class="card-body d-flex justify-content-center">
+                                            <div class="quotes text-center">
+                                        
+                                                Exposure to talent scouts with opportunity to be discovered
+                                                                                      
+                                            </div>
+                                               <img class="" src="img/write5.jpg" width="350px">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="card border-0">
+                                        <div class="card-body d-flex justify-content-center">
+                                            <div class="quotes text-center">
+                                         
+                                                Keep you apprised of future reality productions
+                                                                                        
+                                            </div>
+                                               <img class="" src="img/write3.jpg" width="350px">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span class="fa fa-arrow-circle-o-left fa-2x" aria-hidden="true" style="color : black"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span class="fa fa-arrow-circle-o-right fa-2x" aria-hidden="true" style="color : black"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                        
+                    </div>
+                    
+                
+                </div>
+
+            </section>    
+
+       <?php require "scripts/footer_two.php"; ?>
--- scripts/header_two.php
+++ scripts/header_two.php
@@ -7,14 +7,20 @@
     <link rel="shortcut icon" type="image.png" href="img/logo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:wght@300;400;600;700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
-    <title><?= $title ?? "Realtv Registry"; ?></title>
+    <title><?= $title ?? "Reality TV | RealTVRegistry.com"; ?></title>
 </head>
 <body>
 <style>
+
+.container-fluid{
+    padding : 0 !important;
+    margin : 0 !important;
+  }
     #logo{
         height : 15vh;        
     }
