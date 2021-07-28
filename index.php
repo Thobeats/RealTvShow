eec6af8fe512073e8e8a64cbe85ec732837196e9
firstlincoln <firstlincoln@macs-MacBook-Pro.local>
2021-07-28 23:58:03 +0100

    update

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ require "scripts/header.php";
                         <div class="col-1-of-3">
                             <div class="feature-box">
                                 <img src="img/shoot00.jpg" alt="feature-pic" class="feature-box__img">
-                                <h2 class="heading-secondary"><a href="signup.php" class="feature-box__img--text">INDUSTRY TV EXECUTIVE REGISTRATION</a></h2>
+                                <h2 class="heading-secondary"><a href="Eprojectsreg.php" class="feature-box__img--text">INDUSTRY TV EXECUTIVE REGISTRATION</a></h2>
                             </div>
                         </div>
 
@@ -47,67 +47,84 @@ require "scripts/header.php";
                             Synopsis of 12 Realities
                         </h2>
                     </div>
+                    <!-- <div class="row"> -->
+                    <?php 
+                            $movie_query = mysqli_query($link, "Select * from realtv_movies limit 9");
 
-                    <div class="row">
-                        <div class="col-1-of-3">
-                            <div class="card">
-                                <div class="card__side card__side--front">
-                                    <div class="card__picture card__picture--1">
-                                        <img src="img/snipers.jpg" alt="card-pic" class="card__img">
-                                    </div>
-                                    <h4 class="card__heading">
-                                        <span class="card__heading-span card__heading-span--1">Battle Grounds - Snipers at Large</span>    
-                                    </h4>
-                                </div>
-                                <div class="card__side card__side--back card__side--back-1">
-                                    <div class="card__cta">
-                                        <a href="signup.php" class="bttn bttn--white">Check It Out Now</a>
-                                    </div> 
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-1-of-3">
-                            <div class="card">
-                                <div class="card__side card__side--front">
-                                    <div class="card__picture card__picture--2">
-                                        <img src="img/scene1.jpg" alt="card-pic" class="card__img">
-                                    </div>
-                                    <h4 class="card__heading">
-                                        <span class="card__heading-span card__heading-span--2">Ovations - Sound of Music</span>    
-                                    </h4>
-                                </div>
-                                <div class="card__side card__side--back card__side--back-2">
-                                    <div class="card__cta">
-                                        <a href="signup.php" class="bttn bttn--white">Check It Out Now</a>
-                                    </div> 
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-1-of-3">
-                            <div class="card">
-                                <div class="card__side card__side--front">
-                                    <div class="card__picture card__picture--3">
-                                        <img src="img/lifeboat.jpg" alt="card-pic" class="card__img">
-                                    </div>
-                                    <h4 class="card__heading">
-                                        <span class="card__heading-span card__heading-span--3">Life Boat - Unhinged</span>    
-                                    </h4>
-                                </div>
-                                <div class="card__side card__side--back card__side--back-3">
-                                    <div class="card__cta">
-                                        <a href="signup.php" class="bttn bttn--white">Check It Out Now</a>
-                                    </div> 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                            
+                            $num_rows = $movie_query->num_rows;
+                            if($num_rows > 0){
+                                $row = ceil($num_rows / 3);
+                                $row += 1;
+                                $rowCount = 1; 
+                                $start = 1;
+                                
 
-                    <div class="row">
+                                while ($rowCount < $row){
+                                    $movie_multiple = 3 * $rowCount; 
+                                
+                                    //echo $rowCount;
+                                //mysqli_data_seek($movie_query, $movie_multiple);
+                            ?>
+                            <div class="row"> 
+                            <?php
+                                $cardCount = $start; 
+                            
+                                while($cardCount < $movie_multiple+1){
+                                    
+                                    $movies = mysqli_fetch_assoc($movie_query);
+
+                                    if($movies != NULL){
+                                    $img = $movies['movie_pic']; $title = $movies['movie_title'];
+
+
+                            ?>
+                            <div class="col-1-of-3">
+                                <div class="card">
+                                    <div class="card__side card__side--front">
+                                        <div class="card__picture card__picture--1">
+                                            <img src="img/uploads/<?=$img ?>" alt="card-pic" class="card__img">
+                                        </div>
+                                        <h4 class="card__heading">
+                                            <span class="card__heading-span card__heading-span--1"><?= $title ?></span>    
+                                        </h4>
+                                    </div>
+                                    <div class="card__side card__side--back card__side--back-1">
+                                        <div class="card__cta">
+                                            <a href="signup.php" class="bttn bttn--white">Check It Out Now</a>
+                                        </div> 
+                                    </div>
+                                </div>
+                            </div>
+                            <?php 
+                            }
+
+                            
+                            
+                            
+                                $cardCount++;
+                                if($cardCount % 3 == 0){
+                                    $start = $cardCount + 1;
+                                }
+                            }
+
+                            
+                            
+                            ?>
+                            </div>
+                        <?php $rowCount++;  //$start += $cardCount;
+                            }
+                        
+                        } else { 
+                        
+                        
+                       ?>
+                     <div class="row">
                         <div class="col-1-of-3">
                             <div class="card">
                                 <div class="card__side card__side--front">
                                     <div class="card__picture card__picture--4">
-                                        <img src="img/war.jpg" alt="card-pic" class="card__img">
+                                        <img src="img/oldwar.jpg" alt="card-pic" class="card__img">
                                     </div>
                                     <h4 class="card__heading">
                                         <span class="card__heading-span card__heading-span--4">Battles of Foreign Lands</span>    
@@ -124,7 +141,7 @@ require "scripts/header.php";
                             <div class="card">
                                 <div class="card__side card__side--front">
                                     <div class="card__picture card__picture--5">
-                                        <img src="img/love.jpg" alt="card-pic" class="card__img">
+                                        <img src="img/act3.jpg" alt="card-pic" class="card__img">
                                     </div>
                                     <h4 class="card__heading">
                                         <span class="card__heading-span card__heading-span--5">Modelled to Market</span>    
@@ -155,7 +172,7 @@ require "scripts/header.php";
                             </div>
                         </div>
                     </div>
-
+                    <?php } ?>
                     <div class="u-center-text u-margin-top-big">
                         <a href="projects.php" class="btnn bttn--blue">Discover all realities</a>
                     </div>
@@ -172,64 +189,64 @@ require "scripts/header.php";
                     <button class="w3-button w3-display-right" onclick="plusSlides(+1)">&#10095;</button>
                         <div class="row">
                                 <div class="slideshows" onclick="currentSlide(1)">
-                                    <div class="col-2-of-3">
-                                        <p class="slideshows__paragraph"> Inclusion of your credentials on the RealityTVRegistry platform </p>
+                                    <div class="col-1-of-2">
+                                        <q class="slideshows__paragraph"> Inclusion of your credentials on the RealityTVRegistry platform </q>
                                     </div>
-                                    <div class="col-1-of-3">
+                                    <div class="col-1-of-2">
                                         <img class="slideshows__slides--img" src="img/write01.jpg">
                                     </div>
                                 </div>
                             
                                 <div class="slideshows" style= "display:none;" onclick="currentSlide(2)">
-                                    <div class="col-2-of-3">
-                                        <p class="slideshows__paragraph"> Priority status to participate in the production of your choice </p>
+                                    <div class="col-1-of-2">
+                                        <q class="slideshows__paragraph"> Priority status to participate in the production of your choice </q>
                                     </div>
-                                    <div class="col-1-of-3">
+                                    <div class="col-1-of-2">
                                         <img class="slideshows__slides--img" src="img/shoot2.jpg" >
                                     </div>
                                 </div>
                             
                                 <div class="slideshows" style= "display:none;" onclick="currentSlide(3)">
-                                    <div class="col-2-of-3">
-                                        <p class="slideshows__paragraph"> Your private account access, to view and edit your credentials </p>
+                                    <div class="col-1-of-2">
+                                        <q class="slideshows__paragraph"> Your private account access, to view and edit your credentials </q>
                                     </div>
-                                    <div class="col-1-of-3">
+                                    <div class="col-1-of-2">
                                         <img class="slideshows__slides--img" src="img/Executive.jpg">
                                     </div>
                                 </div>
 
                                 <div class="slideshows" style= "display:none;" onclick="currentSlide(4)">
-                                    <div class="col-2-of-3">
-                                        <p class="slideshows__paragraph"> Promote your individual talents to producers and TV executives </p>
+                                    <div class="col-1-of-2">
+                                        <q class="slideshows__paragraph"> Promote your individual talents to producers and TV executives </q>
                                     </div>
-                                    <div class="col-1-of-3">
+                                    <div class="col-1-of-2">
                                         <img class="slideshows__slides--img" src="img/act000.jpg">
                                     </div>
                                 </div>
 
                                 <div class="slideshows" style= "display:none;" onclick="currentSlide(5)">
-                                    <div class="col-2-of-3">
-                                        <p class="slideshows__paragraph"> Casting companies access your info. and initiate contact  </p>
+                                    <div class="col-1-of-2">
+                                        <q class="slideshows__paragraph"> Casting companies access your info. and initiate contact  </q>
                                     </div>
-                                    <div class="col-1-of-3">
+                                    <div class="col-1-of-2">
                                         <img class="slideshows__slides--img" src="img/studio0.jpg">
                                     </div>
                                 </div>
 
                                 <div class="slideshows" style= "display:none;" onclick="currentSlide(6)">
-                                    <div class="col-2-of-3">
-                                        <p class="slideshows__paragraph"> Exposure to talent scouts with opportunity to be discovered</p>
+                                    <div class="col-1-of-2">
+                                        <q class="slideshows__paragraph"> Exposure to talent scouts with opportunity to be discovered</q>
                                     </div>
-                                    <div class="col-1-of-3">
+                                    <div class="col-1-of-2">
                                         <img class="slideshows__slides--img" src="img/write5.jpg">
                                     </div>
                                 </div>
 
                                 <div class="slideshows" style="display:none;" onclick="currentSlide(7)">
-                                    <div class="col-2-of-3">
-                                        <p class="slideshows__paragraph"> Keep you apprised of future reality productions</p>
+                                    <div class="col-1-of-2">
+                                        <q class="slideshows__paragraph"> Keep you apprised of future reality productions</q>
                                     </div>
-                                    <div class="col-1-of-3">
+                                    <div class="col-1-of-2">
                                         <img class="slideshows__slides--img" src="img/write3.jpg">
                                     </div>
                                 </div>
@@ -238,93 +255,26 @@ require "scripts/header.php";
                 </section>
 
                 <section class="section-billboard">
-                    <div class="container billboard">
-                            <div class="col-2-of-3 billboard__group">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-1-of-2">
+                                <div class="billboard__group">
                                     <p class="billboard__paragraph">
                                         Registered members have <i class="billboard__link">priority</i> to be selected as <i class="billboard__link">participants</i> and to be represented in our Reality TV promotional campaigns.
                                     </p>
                                     <div class="billboard__cta"> 
                                         <a href="signup.php" class="butn butn--white">Get Started</a>
                                     </div>
+                                </div> 
                             </div>
-                            <div class="col-1-of-3">
+                            <div class="col-1-of-2">
                                     <img src="img/act02.jpg" alt="card-pic" class="billboard__img">
                             </div>
+                        </div>
                     </div>
                 </section>
         </main>
 
 
-                <!--
-                <section class="grid-test">
-                        <div class="row">
-                            <div class="col-1-of-2">
-                                Col 1 of 2
-                            </div>
-                            <div class="col-1-of-2">
-                                Col 1 of 2
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-1-of-3">
-                                Col 1 of 3
-                            </div>
-                            <div class="col-1-of-3">
-                                Col 1 of 3
-                            </div>
-                            <div class="col-1-of-3">
-                                Col 1 of 3
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-1-of-3">
-                                Col 1 of 3
-                            </div>
-                            <div class="col-2-of-3">
-                                Col 2 of 3
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-1-of-4">
-                                Col 1 of 4
-                            </div>
-                            <div class="col-1-of-4">
-                                Col 1 of 4
-                            </div>
-                            <div class="col-1-of-4">
-                                Col 1 of 4
-                            </div>
-                            <div class="col-1-of-4">
-                                Col 1 of 4
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-1-of-4">
-                                Col 1 of 4
-                            </div>
-                            <div class="col-1-of-4">
-                                Col 1 of 4
-                            </div>
-                            <div class="col-2-of-4">
-                                Col 2 of 4
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-1-of-4">
-                                Col 1 of 4
-                            </div>
-                            <div class="col-3-of-4">
-                                Col 3 of 4
-                            </div>
-                        </div>
-                </section>
-            -->
+            
 <?php require "scripts/footer.php"; ?>
-
-                    
-    
