ed621e4590161ce05449b197d9d2e479a0ea7f59
firstlincoln <firstlincoln@macs-MacBook-Pro.local>
2021-08-17 14:18:20 +0100

    update contact

--- contactus2.php
+++ contactus2.php
@@ -6,7 +6,7 @@ require "scripts/header_two.php";
 //var_dump($_SESSION);
 ?>
 <style>
-    .cover-image{
+.cover-image{
     background-image: url(img/drone4.jpg);
     height: 100vh;
     background-size : cover;
@@ -14,15 +14,12 @@ require "scripts/header_two.php";
     color : white;
     display: flex;
     align-items : center;
-}
-
-.cover-wrapper{
+}.cover-wrapper{
     text-align : center;
     font-family: "Montserrat", sans-serif;
     width : 100%;
-    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.274) ,rgba(0, 0, 0, 0.219));
+    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.104) ,rgba(0, 0, 0, 0.219));
 }
-
 .cover-wrapper h1{
     font-weight : 600;
     letter-spacing : 1.75rem;
@@ -40,31 +37,43 @@ require "scripts/header_two.php";
     font-family: 'Poppins', serif;
     font-size: 20px; 
     letter-spacing : 3px;   
-}.about-us-text{
+}.contact-us-text{
     font-family: 'Poppins', serif;
-    font-size : 15px;
     letter-spacing : 2px;
     font-weight : 300;
-}.about-us-img{
+}
+
+.contact-us-text h3{
+   font-size : 50px;
+   text-transform : capitalize;
+}
+
+.contact-us-text a {
+   font-size : 15px;
+   display: block;
+   color : #004883;
+}
+.contact-form{
     width : 100%;
-    clip-path : polygon(0% 0%, 0% 100%, 100% 96%, 100% 0%);
-}@media only screen and (max-width: 768px) {.about-us{font-weight : 500;font-size: 15px; }.about-us-text{font-size : 12px;} .img-wrapper{margin-top: 50px;}  .cover-image{height: 80vh;} }
-@media only screen and (max-width: 425px) {body{width : auto;}.about-us{font-weight : 500;font-size: 12px;}
-    .about-us-text{font-size : 10px;}
-    .about-us-img{
-        width: 70%;
+}
+.contact-details-list{
+    list-style: none;
+}
+@media only screen and (max-width: 768px) {.about-us{font-weight : 500;font-size: 17px; }.contact-us-text h3{font-size : 40px;}.contact-us-text a{font-size : 12px;}.img-wrapper{margin-top: 50px;}.cover-image{height: 80vh;} }
+@media only screen and (max-width: 425px) {body{width : auto;}.about-us{font-weight : 500;font-size: 15px;}
+    .contact-us-text h3{font-size : 30px;}
+    .contact-form{
+        width: 90%;
     }
     .cover-image{
         height:50vh;background-position : center right;
     }
     .cover-wrapper h1{
         font-size : 25px;
-        letter-spacing : 2px;
+        letter-spacing : 3px;
     }.cover-wrapper p{
         font-size : 15px;
-        letter-spacing : 1px;
-    }.img-wrapper{
-        margin-top: -30px;
+        letter-spacing : 2px;
     }
 }
 </style>
@@ -79,44 +88,59 @@ require "scripts/header_two.php";
 <section class="about-us-wrapper">
     <div class="row p-3">
         <div class="col-lg-12 d-flex justify-content-center pt-4">
-            <p class="pl-5 about-us text-center"> Please refer to <a href="#">FAQ’s</a> page for quick answers to your question </p>
+            <p class="pl-5 about-us text-center"> Please refer to <a href="faq.php">FAQ’s</a> page for quick answers to your question </p>
         </div>        
     </div>
 
     <div class="row">
         <div class="col-lg-7 col-md-7 col-sm-12">
-            <div class="about-us-text px-5 pb-5 text-center">
-                <p>
-                    In the essence of this community, aspiring individuals of great talent and ambition 
-                    convene, on their paths to discovery. With your commitment for success, we pledge to reveal your individual 
-                    talents in the quest of your discovery.
-                </p>
-
-                <p>
-                    Our philosophy is simple. Attaining success for talent suspended in anonymity, can 
-                    simply result in that individual aligning with a source who possesses relentless drive to promote their talent 
-                    until the dream is realized.
-                </p>
-
-                <p>
-                    The founder is known for his marketing and promotional skills. With more than 35 
-                    years-experience in developing, targeting and implementing successful marketing strategies, the founder has 
-                    secured multi-year commitments with hundreds of companies and Fortune 500 entities located within the continental 
-                    United States and Hawaii.
-                </p>
-                <p>
-                With the many forms of advertising mediums, selecting an effective mix is essential if 
-                            success is to be achieved. As the founder, I pledge that the staff at Reality TV Registry is committed in advancing 
-                            your talent and success.  
-                            Not to be overstated, this highly diverse destination has been developed to elevate our client’s visibility and 
-                            promote their skills, whether it be their special talents, competitiveness or unique reality proposals.
-                </p>
+            <div class="contact-us-text px-3 pt-5">
+                <h3 class="my-5 text-center">Stay connected as we build a community of reality.</h3>
+                <a class="text-left" href="#">
+                    <i class="bi bi-envelope-fill"></i> Connect@RealityTVRegistry.com
+                </a>
+                <a class="text-left" href="#">
+                    <i class="bi bi-envelope-fill"></i> PaulM@RealityTVRegistry.com
+                </a>
+                <a class="text-left" href="#">
+                    <i class="bi bi-geo-alt-fill"></i> 626, Wilshire Blvd Ste: 410 Los Angeles,CA. 90017
+                </a>
             </div>
         </div>
 
         <div class="col-lg-4 col-md-5 col-sm-12 p-0 mb-5">
-            <div class="p-2 img-wrapper text-center">
-                <img src="img/shoot7.png" alt="" class="about-us-img">
+            <div class="p-2 img-wrapper d-flex justify-content-center">
+                <form action="" method="POST" class="contact-form">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Email</label>
+                        <input type="email" name="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Interested As</label>
+                        <select name="interested_as" class="form-control" id="">
+                            <option>Select Role</option>
+                            <?php 
+                            $roleq = mysqli_query($link, "select * from realtv_roles");  
+
+                            while($role = mysqli_fetch_assoc($roleq)):
+                            
+                            ?>
+
+                                <option value="<?= $role['id'] ?>"><?= ucfirst($role['role_name']) ?></option>
+
+                            <?php endwhile; ?>     
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Message</label>
+                        <textarea name="message" id="" class="form-control" cols="30" rows="10"></textarea>
+                    </div>
+                    <button class="realbtn btn-warning" name="deliver">Deliver</button>
+                </form>
             </div>
         </div>
     </div>
