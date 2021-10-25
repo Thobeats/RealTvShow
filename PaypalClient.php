3c4ac9efa289641ff6b96d6a8af1a497eb57fbb6
firstlincoln <firstlincoln@macs-MacBook-Pro.local>
2021-10-25 10:46:52 +0100

    update

--- PaypalClient.php
+++ PaypalClient.php
@@ -3,8 +3,8 @@
 namespace Sample;
 
 use PayPalCheckoutSdk\Core\PayPalHttpClient;
-use PayPalCheckoutSdk\Core\SandboxEnvironment;
-//use PayPalCheckoutSdk\Core\LiveEnvironment;
+//use PayPalCheckoutSdk\Core\SandboxEnvironment;
+use PayPalCheckoutSdk\Core\LiveEnvironment;
 
 ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
 ini_set('display_errors', '1');
@@ -28,8 +28,10 @@ class PayPalClient
      */
     public static function environment()
     {
-        $clientId = getenv("CLIENT_ID") ?: "AfgYATIfVWBGwQVce9ggpT8F3cpdMckdMmaf525u6IvyLjD1oL8RTiTqHVZrUWMvn7Un6r2q_qDehBJY";
-        $clientSecret = getenv("CLIENT_SECRET") ?: "ECi9S1n5jjMDebCT9tRhvXyCpCrM60J9elBFD1ostrPT5StduTQ5SSv69-mqIduFPjrdwaaCbo0ibVP2";
+        $liveid = "Ab6HlNbUVsy9Qz90YXanfOquyv7qZdtHQxbrzqY5ZASTvZdm9bfRyHi72UrBwO6Q_hW8WtgeN0NnieGH"; $livesec = "EG4ksmbvNNXUxgnJHcbzEtwmejlxMUZMFrUwZTyKP7mYJ5bZLTDlh8t1sv0VfkQ5L7-r0J67EM1zLSFc";
+        $sandbox = "AfgYATIfVWBGwQVce9ggpT8F3cpdMckdMmaf525u6IvyLjD1oL8RTiTqHVZrUWMvn7Un6r2q_qDehBJY"; $sandboxsec = "ECi9S1n5jjMDebCT9tRhvXyCpCrM60J9elBFD1ostrPT5StduTQ5SSv69-mqIduFPjrdwaaCbo0ibVP2";
+        $clientId = getenv("CLIENT_ID") ?: $liveid;
+        $clientSecret = getenv("CLIENT_SECRET") ?: $livesec;
         return new SandboxEnvironment($clientId, $clientSecret);
     }
 }
