d8fb01df5ccd76f9a36e956f7f4b6d741aa0f36d
firstlincoln <firstlincoln@macs-MacBook-Pro.local>
2021-10-26 22:38:47 +0100

    update

--- PaypalClient.php
+++ PaypalClient.php
@@ -4,7 +4,7 @@ namespace Sample;
 
 use PayPalCheckoutSdk\Core\PayPalHttpClient;
 //use PayPalCheckoutSdk\Core\SandboxEnvironment;
-use PayPalCheckoutSdk\Core\LiveEnvironment;
+use PayPalCheckoutSdk\Core\ProductionEnvironment;
 
 ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
 ini_set('display_errors', '1');
@@ -32,6 +32,6 @@ class PayPalClient
         $sandbox = "AfgYATIfVWBGwQVce9ggpT8F3cpdMckdMmaf525u6IvyLjD1oL8RTiTqHVZrUWMvn7Un6r2q_qDehBJY"; $sandboxsec = "ECi9S1n5jjMDebCT9tRhvXyCpCrM60J9elBFD1ostrPT5StduTQ5SSv69-mqIduFPjrdwaaCbo0ibVP2";
         $clientId = getenv("CLIENT_ID") ?: $liveid;
         $clientSecret = getenv("CLIENT_SECRET") ?: $livesec;
-        return new SandboxEnvironment($clientId, $clientSecret);
+        return new ProductionEnvironment($clientId, $clientSecret);
     }
 }
--- payments/db.php
+++ payments/db.php
@@ -1,7 +1,7 @@
 <?php 
 
-//$con =  new mysqli('127.0.0.1', 'root', '', 'realtvshow'); 
-$con = new mysqli("localhost", "realtv_db_user", "@lphA3ch0#", "realtv_show");
+$con =  new mysqli('127.0.0.1', 'root', '', 'realtvshow'); 
+//$con = new mysqli("localhost", "realtv_db_user", "@lphA3ch0#", "realtv_show");
 
 
 ?>
